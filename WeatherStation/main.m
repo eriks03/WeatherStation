d8a03ec7a46bdf525801000c07b1fb5b1b3b365a
Erik Schubert <erikschubert@live.com>
2011-09-15 16:09:25 +0200

    Initial Commit

---
+++ WeatherStation/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  WeatherStation
+//
+//  Created by Erik Schubert on 15.09.11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
